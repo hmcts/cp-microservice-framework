CJSCommonPlatform/microservice-framework

Add new Optional String parameter to Jmx Commands

framework-jmx-command-client/src/main/java/uk/gov/justice/framework/command/client/jmx/SystemCommandInvoker.java
@@ -5,14 +5,13 @@ import uk.gov.justice.framework.command.client.io.ToConsolePrinter;
 import uk.gov.justice.services.jmx.api.SystemCommandInvocationFailedException;
 import uk.gov.justice.services.jmx.api.UnrunnableSystemCommandException;
 import uk.gov.justice.services.jmx.api.mbean.CommandRunMode;
-import uk.gov.justice.services.jmx.api.mbean.SystemCommanderMBean;
+import uk.gov.justice.services.jmx.api.mbean.JmxCommandMBean;
+import uk.gov.justice.services.jmx.api.parameters.JmxCommandRuntimeParameters;
 import uk.gov.justice.services.jmx.system.command.client.SystemCommanderClient;
 import uk.gov.justice.services.jmx.system.command.client.SystemCommanderClientFactory;
 import uk.gov.justice.services.jmx.system.command.client.connection.JmxParameters;
 
 import java.util.UUID;
-
-import org.apache.commons.lang3.StringUtils;
 
 public class SystemCommandInvoker {
 
@@ -29,7 +28,11 @@ public class SystemCommandInvoker {
         this.toConsolePrinter = toConsolePrinter;
     }
 
-    public void runSystemCommand(final String commandName, final JmxParameters jmxParameters, final String commandRuntimeId, final CommandRunMode commandRunMode) {
+    public void runSystemCommand(
+            final String commandName,
+            final JmxParameters jmxParameters,
+            final JmxCommandRuntimeParameters jmxCommandRuntimeParameters,
+            final CommandRunMode commandRunMode) {
 
         final String contextName = jmxParameters.getContextName();
 
@@ -42,10 +45,10 @@ public class SystemCommandInvoker {
 
             toConsolePrinter.printf("Connected to %s context", contextName);
 
-            final SystemCommanderMBean systemCommanderMBean = systemCommanderClient.getRemote(contextName);
-            final UUID commandId = invoke(systemCommanderMBean, commandName, commandRuntimeId, commandRunMode);
+            final JmxCommandMBean jmxCommandMBean = systemCommanderClient.getRemote(contextName);
+            final UUID commandId = jmxCommandMBean.call(commandName, jmxCommandRuntimeParameters, commandRunMode);
             toConsolePrinter.printf("System command '%s' with id '%s' successfully sent to %s", commandName, commandId, contextName);
-            commandPoller.runUntilComplete(systemCommanderMBean, commandId, commandName);
+            commandPoller.runUntilComplete(jmxCommandMBean, commandId, commandName);
 
         } catch (final UnrunnableSystemCommandException e) {
             toConsolePrinter.printf("The command '%s' is not supported on this %s context", commandName, contextName);
@@ -60,19 +63,4 @@ public class SystemCommandInvoker {
         }
     }
 
-    private UUID invoke(final SystemCommanderMBean systemCommanderMBean, final String commandName, final String commandRuntimeId, final CommandRunMode commandRunMode) {
-        if(StringUtils.isEmpty(commandRuntimeId)) {
-            return systemCommanderMBean.call(commandName, commandRunMode);
-        } else {
-            return systemCommanderMBean.callWithRuntimeId(commandName, convertToUUID(commandRuntimeId), commandRunMode);
-        }
-    }
-
-    private UUID convertToUUID(String id) {
-        try{
-            return UUID.fromString(id);
-        } catch(IllegalArgumentException e) {
-            throw new CommandLineException("Unable to invoke command as supplied commandRuntimeId is not uuid format: " + id, e);
-        }
-    }
 }

jmx/jmx-command-handling/src/test/java/uk/gov/justice/services/jmx/MBeanInstantiatorTest.java
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
-import uk.gov.justice.services.jmx.api.mbean.SystemCommander;
+import uk.gov.justice.services.jmx.api.mbean.DefaultJmxCommandMBean;
 import uk.gov.justice.services.jmx.api.name.CommandMBeanNameProvider;
 import uk.gov.justice.services.jmx.api.name.ObjectNameException;
 import uk.gov.justice.services.jmx.util.ContextNameProvider;
@@ -33,7 +33,7 @@ public class MBeanInstantiatorTest {
     private MBeanServer mbeanServer;
 
     @Mock
-    private SystemCommander systemCommander;
+    private DefaultJmxCommandMBean systemCommander;
 
     @Mock
     private ContextNameProvider contextNameProvider;
@@ -60,7 +60,7 @@ public class MBeanInstantiatorTest {
         mBeanInstantiator.registerSystemCommanderMBean();
 
         verify(mbeanServer).registerMBean(systemCommander, objectName);
-        verify(logger).info("Registering SystemCommander MBean using name 'mBeanName'");
+        verify(logger).info("Registering JMX mBean class 'DefaultJmxCommandMBean' using name 'mBeanName'");
     }
 
     @Test
@@ -113,7 +113,7 @@ public class MBeanInstantiatorTest {
         mBeanInstantiator.unregisterMBeans();
 
         verify(mbeanServer).unregisterMBean(objectName);
-        verify(logger).info("Unregistering SystemCommander MBean using name 'mBeanName'");
+        verify(logger).info("Unregistering JMX MBean class 'DefaultJmxCommandMBean' using name 'mBeanName'");
     }
 
     @Test

test-utils/test-utils-jmx/src/test/java/uk/gov/justice/services/jmx/system/command/client/FrameworkSystemCommandCallerTest.java
@@ -12,7 +12,8 @@ import static uk.gov.justice.services.management.suspension.commands.UnsuspendCo
 import static uk.gov.justice.services.test.utils.common.host.TestHostProvider.getHost;
 import static uk.gov.justice.services.test.utils.core.reflection.ReflectionUtil.getValueOfField;
 
-import uk.gov.justice.services.jmx.api.mbean.SystemCommanderMBean;
+import uk.gov.justice.services.jmx.api.mbean.JmxCommandMBean;
+import uk.gov.justice.services.jmx.api.parameters.JmxCommandRuntimeParameters;
 import uk.gov.justice.services.jmx.system.command.client.connection.Credentials;
 import uk.gov.justice.services.jmx.system.command.client.connection.JmxParameters;
 
@@ -35,18 +36,22 @@ public class FrameworkSystemCommandCallerTest {
         final String contextName = "contextName";
 
         final JmxParameters jmxParameters = mock(JmxParameters.class);
+        final JmxCommandRuntimeParameters jmxCommandRuntimeParameters = mock(JmxCommandRuntimeParameters.class);
         final SystemCommanderClient systemCommanderClient = mock(SystemCommanderClient.class);
-        final SystemCommanderMBean systemCommanderMBean = mock(SystemCommanderMBean.class);
+        final JmxCommandMBean jmxCommandMBean = mock(JmxCommandMBean.class);
 
-        final FrameworkSystemCommandCaller frameworkSystemCommandCaller = new FrameworkSystemCommandCaller(jmxParameters, testSystemCommanderClientFactory);
+        final FrameworkSystemCommandCaller frameworkSystemCommandCaller = new FrameworkSystemCommandCaller(
+                jmxParameters,
+                jmxCommandRuntimeParameters,
+                testSystemCommanderClientFactory);
 
         when(jmxParameters.getContextName()).thenReturn(contextName);
         when(testSystemCommanderClientFactory.create(jmxParameters)).thenReturn(systemCommanderClient);
-        when(systemCommanderClient.getRemote(contextName)).thenReturn(systemCommanderMBean);
+        when(systemCommanderClient.getRemote(contextName)).thenReturn(jmxCommandMBean);
 
         frameworkSystemCommandCaller.callShutter();
 
-        verify(systemCommanderMBean).call(SUSPEND, GUARDED);
+        verify(jmxCommandMBean).call(SUSPEND, jmxCommandRuntimeParameters, GUARDED);
         verify(systemCommanderClient).close();
     }
 
@@ -56,18 +61,22 @@ public class FrameworkSystemCommandCallerTest {
         final String contextName = "contextName";
 
         final JmxParameters jmxParameters = mock(JmxParameters.class);
+        final JmxCommandRuntimeParameters jmxCommandRuntimeParameters = mock(JmxCommandRuntimeParameters.class);
         final SystemCommanderClient systemCommanderClient = mock(SystemCommanderClient.class);
-        final SystemCommanderMBean systemCommanderMBean = mock(SystemCommanderMBean.class);
+        final JmxCommandMBean jmxCommandMBean = mock(JmxCommandMBean.class);
 
-        final FrameworkSystemCommandCaller frameworkSystemCommandCaller = new FrameworkSystemCommandCaller(jmxParameters, testSystemCommanderClientFactory);
+        final FrameworkSystemCommandCaller frameworkSystemCommandCaller = new FrameworkSystemCommandCaller(
+                jmxParameters,
+                jmxCommandRuntimeParameters,
+                testSystemCommanderClientFactory);
 
         when(jmxParameters.getContextName()).thenReturn(contextName);
         when(testSystemCommanderClientFactory.create(jmxParameters)).thenReturn(systemCommanderClient);
-        when(systemCommanderClient.getRemote(contextName)).thenReturn(systemCommanderMBean);
+        when(systemCommanderClient.getRemote(contextName)).thenReturn(jmxCommandMBean);
 
         frameworkSystemCommandCaller.callUnshutter();
 
-        verify(systemCommanderMBean).call(UNSUSPEND, GUARDED);
+        verify(jmxCommandMBean).call(UNSUSPEND, jmxCommandRuntimeParameters, GUARDED);
         verify(systemCommanderClient).close();
     }
 

hmcts/cp-microservice-framework

framework-jmx-command-client/src/main/java/uk/gov/justice/framework/command/client/jmx/SystemCommandInvoker.java
@@ -6,13 +6,12 @@ import uk.gov.justice.services.jmx.api.SystemCommandInvocationFailedException;
 import uk.gov.justice.services.jmx.api.UnrunnableSystemCommandException;
 import uk.gov.justice.services.jmx.api.mbean.CommandRunMode;
 import uk.gov.justice.services.jmx.api.mbean.SystemCommanderMBean;
+import uk.gov.justice.services.jmx.api.parameters.JmxCommandRuntimeParameters;
 import uk.gov.justice.services.jmx.system.command.client.SystemCommanderClient;
 import uk.gov.justice.services.jmx.system.command.client.SystemCommanderClientFactory;
 import uk.gov.justice.services.jmx.system.command.client.connection.JmxParameters;
 
 import java.util.UUID;
-
-import org.apache.commons.lang3.StringUtils;
 
 public class SystemCommandInvoker {
 
@@ -29,7 +28,11 @@ public class SystemCommandInvoker {
         this.toConsolePrinter = toConsolePrinter;
     }
 
-    public void runSystemCommand(final String commandName, final JmxParameters jmxParameters, final String commandRuntimeId, final CommandRunMode commandRunMode) {
+    public void runSystemCommand(
+            final String commandName,
+            final JmxParameters jmxParameters,
+            final JmxCommandRuntimeParameters jmxCommandRuntimeParameters,
+            final CommandRunMode commandRunMode) {
 
         final String contextName = jmxParameters.getContextName();
 
@@ -42,10 +45,10 @@ public class SystemCommandInvoker {
 
             toConsolePrinter.printf("Connected to %s context", contextName);
 
-            final SystemCommanderMBean systemCommanderMBean = systemCommanderClient.getRemote(contextName);
-            final UUID commandId = invoke(systemCommanderMBean, commandName, commandRuntimeId, commandRunMode);
+            final SystemCommanderMBean jmxCommandMBean = systemCommanderClient.getRemote(contextName);
+            final UUID commandId = jmxCommandMBean.call(commandName, jmxCommandRuntimeParameters, commandRunMode);
             toConsolePrinter.printf("System command '%s' with id '%s' successfully sent to %s", commandName, commandId, contextName);
-            commandPoller.runUntilComplete(systemCommanderMBean, commandId, commandName);
+            commandPoller.runUntilComplete(jmxCommandMBean, commandId, commandName);
 
         } catch (final UnrunnableSystemCommandException e) {
             toConsolePrinter.printf("The command '%s' is not supported on this %s context", commandName, contextName);
@@ -60,19 +63,4 @@ public class SystemCommandInvoker {
         }
     }
 
-    private UUID invoke(final SystemCommanderMBean systemCommanderMBean, final String commandName, final String commandRuntimeId, final CommandRunMode commandRunMode) {
-        if(StringUtils.isEmpty(commandRuntimeId)) {
-            return systemCommanderMBean.call(commandName, commandRunMode);
-        } else {
-            return systemCommanderMBean.callWithRuntimeId(commandName, convertToUUID(commandRuntimeId), commandRunMode);
-        }
-    }
-
-    private UUID convertToUUID(String id) {
-        try{
-            return UUID.fromString(id);
-        } catch(IllegalArgumentException e) {
-            throw new CommandLineException("Unable to invoke command as supplied commandRuntimeId is not uuid format: " + id, e);
-        }
-    }
 }

jmx/jmx-command-handling/src/test/java/uk/gov/justice/services/jmx/MBeanInstantiatorTest.java
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
-import uk.gov.justice.services.jmx.api.mbean.SystemCommander;
+import uk.gov.justice.services.jmx.api.mbean.SystemCommanderMBean;
 import uk.gov.justice.services.jmx.api.name.CommandMBeanNameProvider;
 import uk.gov.justice.services.jmx.api.name.ObjectNameException;
 import uk.gov.justice.services.jmx.util.ContextNameProvider;
@@ -33,7 +33,7 @@ public class MBeanInstantiatorTest {
     private MBeanServer mbeanServer;
 
     @Mock
-    private SystemCommander systemCommander;
+    private SystemCommanderMBean systemCommander;
 
     @Mock
     private ContextNameProvider contextNameProvider;
@@ -60,7 +60,7 @@ public class MBeanInstantiatorTest {
         mBeanInstantiator.registerSystemCommanderMBean();
 
         verify(mbeanServer).registerMBean(systemCommander, objectName);
-        verify(logger).info("Registering SystemCommander MBean using name 'mBeanName'");
+        verify(logger).info("Registering JMX mBean class 'SystemCommanderMBean' using name 'mBeanName'");
     }
 
     @Test
@@ -113,7 +113,7 @@ public class MBeanInstantiatorTest {
         mBeanInstantiator.unregisterMBeans();
 
         verify(mbeanServer).unregisterMBean(objectName);
-        verify(logger).info("Unregistering SystemCommander MBean using name 'mBeanName'");
+        verify(logger).info("Unregistering JMX MBean class 'SystemCommanderMBean' using name 'mBeanName'");
     }
 
     @Test

test-utils/test-utils-jmx/src/test/java/uk/gov/justice/services/jmx/system/command/client/FrameworkSystemCommandCallerTest.java
@@ -13,6 +13,7 @@ import static uk.gov.justice.services.test.utils.common.host.TestHostProvider.ge
 import static uk.gov.justice.services.test.utils.core.reflection.ReflectionUtil.getValueOfField;
 
 import uk.gov.justice.services.jmx.api.mbean.SystemCommanderMBean;
+import uk.gov.justice.services.jmx.api.parameters.JmxCommandRuntimeParameters;
 import uk.gov.justice.services.jmx.system.command.client.connection.Credentials;
 import uk.gov.justice.services.jmx.system.command.client.connection.JmxParameters;
 
@@ -35,10 +36,14 @@ public class FrameworkSystemCommandCallerTest {
         final String contextName = "contextName";
 
         final JmxParameters jmxParameters = mock(JmxParameters.class);
+        final JmxCommandRuntimeParameters jmxCommandRuntimeParameters = mock(JmxCommandRuntimeParameters.class);
         final SystemCommanderClient systemCommanderClient = mock(SystemCommanderClient.class);
         final SystemCommanderMBean systemCommanderMBean = mock(SystemCommanderMBean.class);
 
-        final FrameworkSystemCommandCaller frameworkSystemCommandCaller = new FrameworkSystemCommandCaller(jmxParameters, testSystemCommanderClientFactory);
+        final FrameworkSystemCommandCaller frameworkSystemCommandCaller = new FrameworkSystemCommandCaller(
+                jmxParameters,
+                jmxCommandRuntimeParameters,
+                testSystemCommanderClientFactory);
 
         when(jmxParameters.getContextName()).thenReturn(contextName);
         when(testSystemCommanderClientFactory.create(jmxParameters)).thenReturn(systemCommanderClient);
@@ -46,7 +51,7 @@ public class FrameworkSystemCommandCallerTest {
 
         frameworkSystemCommandCaller.callShutter();
 
-        verify(systemCommanderMBean).call(SUSPEND, GUARDED);
+        verify(systemCommanderMBean).call(SUSPEND, jmxCommandRuntimeParameters, GUARDED);
         verify(systemCommanderClient).close();
     }
 
@@ -56,10 +61,14 @@ public class FrameworkSystemCommandCallerTest {
         final String contextName = "contextName";
 
         final JmxParameters jmxParameters = mock(JmxParameters.class);
+        final JmxCommandRuntimeParameters jmxCommandRuntimeParameters = mock(JmxCommandRuntimeParameters.class);
         final SystemCommanderClient systemCommanderClient = mock(SystemCommanderClient.class);
         final SystemCommanderMBean systemCommanderMBean = mock(SystemCommanderMBean.class);
 
-        final FrameworkSystemCommandCaller frameworkSystemCommandCaller = new FrameworkSystemCommandCaller(jmxParameters, testSystemCommanderClientFactory);
+        final FrameworkSystemCommandCaller frameworkSystemCommandCaller = new FrameworkSystemCommandCaller(
+                jmxParameters,
+                jmxCommandRuntimeParameters,
+                testSystemCommanderClientFactory);
 
         when(jmxParameters.getContextName()).thenReturn(contextName);
         when(testSystemCommanderClientFactory.create(jmxParameters)).thenReturn(systemCommanderClient);
@@ -67,7 +76,7 @@ public class FrameworkSystemCommandCallerTest {
 
         frameworkSystemCommandCaller.callUnshutter();
 
-        verify(systemCommanderMBean).call(UNSUSPEND, GUARDED);
+        verify(systemCommanderMBean).call(UNSUSPEND, jmxCommandRuntimeParameters, GUARDED);
         verify(systemCommanderClient).close();
     }
 