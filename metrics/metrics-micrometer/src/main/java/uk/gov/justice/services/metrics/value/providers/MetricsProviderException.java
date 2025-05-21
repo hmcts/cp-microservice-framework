42f702041e6783a22f6c212c655b46059e989b67
allanmckenzie <allan.mckenzie@hmcts.net>
2025-05-21 12:59:43 +0100

    Add Micrometer Counter Metrics

--- metrics/metrics-micrometer/src/main/java/uk/gov/justice/services/metrics/value/providers/MetricsProviderException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.services.metrics.value.providers;
-
-public class MetricsProviderException extends RuntimeException {
-
-    public MetricsProviderException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-}
