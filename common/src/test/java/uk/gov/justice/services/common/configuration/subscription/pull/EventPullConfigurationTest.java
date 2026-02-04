6389541285ccbfa60d00f8ba162a296ff8c6b69a
Oktay Ekincioglu <oktay.ekincioglu1@hmcts.net>
2026-02-04 12:44:24 +0000

    EventPullConfiguration class added back

---
+++ common/src/test/java/uk/gov/justice/services/common/configuration/subscription/pull/EventPullConfigurationTest.java
@@ -0,0 +1,44 @@
+package uk.gov.justice.services.common.configuration.subscription.pull;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static uk.gov.justice.services.test.utils.core.reflection.ReflectionUtil.setField;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EventPullConfigurationTest {
+
+    @InjectMocks
+    private EventPullConfiguration eventPullConfiguration;
+
+    @Test
+    public void shouldParseTheJndiValueAsBooleanTrueIfTheConfigStringIsTrue() throws Exception {
+
+        setField(eventPullConfiguration, "shouldProcessEventsFromEventTopic", "true");
+        assertThat(eventPullConfiguration.shouldProcessEventsByPullMechanism(), is(true));
+    }
+
+    @Test
+    public void shouldParseTheJndiValueAsBooleanFalseIfTheConfigStringIsNotTrue() throws Exception {
+
+        setField(eventPullConfiguration, "shouldProcessEventsFromEventTopic", "something-not-true");
+        assertThat(eventPullConfiguration.shouldProcessEventsByPullMechanism(), is(false));
+    }
+
+    @Test
+    public void shouldCacheTheParsedBooleanInMemoryOnceParsed() throws Exception {
+
+        setField(eventPullConfiguration, "shouldProcessEventsFromEventTopic", "true");
+        assertThat(eventPullConfiguration.shouldProcessEventsByPullMechanism(), is(true));
+        setField(eventPullConfiguration, "shouldProcessEventsFromEventTopic", "false");
+        assertThat(eventPullConfiguration.shouldProcessEventsByPullMechanism(), is(true));
+        setField(eventPullConfiguration, "shouldProcessEventsFromEventTopic", "something-silly");
+        assertThat(eventPullConfiguration.shouldProcessEventsByPullMechanism(), is(true));
+        setField(eventPullConfiguration, "shouldProcessEventsFromEventTopic", null);
+        assertThat(eventPullConfiguration.shouldProcessEventsByPullMechanism(), is(true));
+    }
+}
