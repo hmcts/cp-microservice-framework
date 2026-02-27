4543c14280d8143fb9a487c099ce6526e6f457a2
zeldal-hmcts <zeldal.ozdemir1@hmcts.net>
2026-02-27 15:11:14 +0000

    PEG-2910 Savepoint-Based Stream Error Recovery

--- persistence/persistence-deltaspike/src/main/java/uk/gov/justice/services/persistence/EntityManagerFlushInterceptor.java
+++ persistence/persistence-deltaspike/src/main/java/uk/gov/justice/services/persistence/EntityManagerFlushInterceptor.java
@@ -7,23 +7,32 @@ import uk.gov.justice.services.core.interceptor.InterceptorChain;
 import uk.gov.justice.services.core.interceptor.InterceptorContext;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
+
+import org.hibernate.Session;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventSource;
+import org.hibernate.event.spi.EventType;
+import org.hibernate.event.spi.FlushEvent;
+import org.hibernate.event.spi.FlushEventListener;
 
 public class EntityManagerFlushInterceptor implements Interceptor {
 
     @Inject
-    private EntityManager entityManager;
+    private HibernateSessionHolder hibernateSessionHolder;
 
     @Inject
     private UserTransaction userTransaction;
 
     @Override
     public InterceptorContext process(final InterceptorContext interceptorContext, final InterceptorChain interceptorChain) {
+        SafeAutoFlushEventListener.enable();
         try {
             return interceptorChain.processNext(interceptorContext);
         } finally {
+            SafeAutoFlushEventListener.disable();
             flushEntityManager();
         }
     }
@@ -31,7 +40,21 @@ public class EntityManagerFlushInterceptor implements Interceptor {
     private void flushEntityManager() {
         try {
             if (userTransaction.getStatus() == STATUS_ACTIVE) {
-                entityManager.flush();
+                final Session session = hibernateSessionHolder.getSession();
+                if (session != null) {
+                    final EventSource eventSource = (EventSource) session;
+                    final FlushEvent flushEvent = new FlushEvent(eventSource);
+
+                    final SessionFactoryImplementor sfi = (SessionFactoryImplementor) session.getSessionFactory();
+                    final Iterable<FlushEventListener> listeners = sfi.getServiceRegistry()
+                            .getService(EventListenerRegistry.class)
+                            .getEventListenerGroup(EventType.FLUSH)
+                            .listeners();
+
+                    for (final FlushEventListener listener : listeners) {
+                        listener.onFlush(flushEvent);
+                    }
+                }
             }
         } catch (final SystemException e) {
             throw new EntityManagerFlushException("Failed to get status of UserTransaction", e);
--- persistence/persistence-deltaspike/src/main/java/uk/gov/justice/services/persistence/EntityManagerProducer.java
+++ persistence/persistence-deltaspike/src/main/java/uk/gov/justice/services/persistence/EntityManagerProducer.java
@@ -2,13 +2,20 @@ package uk.gov.justice.services.persistence;
 
 import java.util.TimeZone;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
+
+import org.hibernate.Session;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
 
 /**
  * Producer of {:link EntityManager} for use with JPA (Delta-spike).
@@ -20,11 +27,30 @@ public class EntityManagerProducer {
     @PersistenceUnit
     EntityManagerFactory entityManagerFactory;
 
+    @Inject
+    HibernateSessionHolder hibernateSessionHolder;
+
+    @PostConstruct
+    private void registerSafeAutoFlushListener() {
+        final EntityManager tempEm = entityManagerFactory.createEntityManager();
+        try {
+            final Session session = tempEm.unwrap(Session.class);
+            final SessionFactoryImplementor sfi = (SessionFactoryImplementor) session.getSessionFactory();
+            final EventListenerRegistry registry = sfi.getServiceRegistry()
+                    .getService(EventListenerRegistry.class);
+            registry.setListeners(EventType.AUTO_FLUSH, new SafeAutoFlushEventListener());
+        } finally {
+            tempEm.close();
+        }
+    }
+
     @Produces
     @RequestScoped
     public EntityManager create() {
         TimeZone.setDefault(TimeZone.getTimeZone(UTC));
-        return entityManagerFactory.createEntityManager();
+        final EntityManager em = entityManagerFactory.createEntityManager();
+        hibernateSessionHolder.setSession(em.unwrap(Session.class));
+        return em;
     }
 
     public void close(@Disposes final EntityManager em) {
